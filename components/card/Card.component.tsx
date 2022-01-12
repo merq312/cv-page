c66c3705b61df1433116a4c91d32c62fe52e474a
merq312 <merc312@gmail.com>
2022-01-12 00:08:40 -0500

    feat: add icon for back button

--- components/card/Card.component.tsx
+++ components/card/Card.component.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 import styles from '../../styles/Card.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons'
 
 const Card = ({
   children,
@@ -11,6 +13,7 @@ const Card = ({
   <div className={styles.card}>
     <div className={styles.content}>
       <div className={styles.backButton} onClick={() => handleClick('Home')}>
+        <FontAwesomeIcon className={styles.icon} icon={faLongArrowAltLeft} />
         Go Back
       </div>
       {children}
